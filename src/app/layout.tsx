32725dea906f9c2e4f8117fc4b5e458b27563134
louispelarrey <louispelarrey@gmail.com>
2023-11-13 04:15:43 +0100

    Remove unused font variable in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import Providers from "./providers";
 
 export const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -23,7 +22,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          inter.variable
+          inter
         )}
       >
         <ThemeProvider

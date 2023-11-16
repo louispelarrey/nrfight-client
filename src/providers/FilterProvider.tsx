6c1db1d741b75d9b54cfb714f6f6a4eaadee3e20
louispelarrey <louispelarrey@gmail.com>
2023-11-16 02:04:00 +0100

    Refactor FilterProvider component to include ReservedCourses interface

--- src/providers/FilterProvider.tsx
+++ src/providers/FilterProvider.tsx
@@ -1,35 +1,48 @@
-"use client"
+"use client";
 
-import useRetreiveReservations from '@/hooks/useRetreiveReservations';
-import { createContext, useState } from 'react';
-import { DateRange } from 'react-day-picker';
+import { PropsWithChildren, createContext, useState } from "react";
+import { DateRange } from "react-day-picker";
+
+export interface ReservedCourses {
+  room: string;
+  reservedCourses: string[];
+}
 
 export interface FilterContext {
-  reservedCourses: string[]
+  // reservedCourses.republique.reservedCourses
+  reservedCourses: ReservedCourses
   setReservedCourses: (reservedCourses: string[]) => void;
-  
+
   excludedDates: DateRange[];
   setExcludedDates: (excludedDates: DateRange[]) => void;
 }
 
 export const FilterContext = createContext({
-  reservedCourses: [""],
+  reservedCourses: [{}],
   setReservedCourses: (reservedCourses: string[]) => {},
 
-  excludedDates: [{
-    from: undefined,
-    to: undefined,
-  } as DateRange],
+  excludedDates: [
+    {
+      from: undefined,
+      to: undefined,
+    } as DateRange,
+  ],
   setExcludedDates: (excludedDates: DateRange[]) => {},
 });
 
-export const FilterProvider = ({ children }: any) => {
-
+export const FilterProvider = ({ children }: PropsWithChildren) => {
   const [reservedCourses, setReservedCourses] = useState<string[]>([]);
   const [excludedDates, setExcludedDates] = useState<DateRange[]>([]);
 
   return (
-    <FilterContext.Provider value={{ reservedCourses, setReservedCourses, excludedDates, setExcludedDates }}>
+    <FilterContext.Provider
+      value={{
+        reservedCourses,
+        setReservedCourses,
+        excludedDates,
+        setExcludedDates,
+      }}
+    >
       {children}
     </FilterContext.Provider>
   );

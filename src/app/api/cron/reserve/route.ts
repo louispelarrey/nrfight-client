9235cf402ddf1ddffb6dba9d39e3e2cb4a50f1bc
louispelarrey <louispelarrey@gmail.com>
2023-11-22 00:11:14 +0100

    test edge

--- src/app/api/cron/reserve/route.ts
+++ src/app/api/cron/reserve/route.ts
@@ -9,6 +9,7 @@ type Data = {
 }[];
 
 export const dynamic = 'force-dynamic'
+export const runtime = 'edge' // 'nodejs' (default) | 'edge'
 
 export async function GET(): Promise<Response> {
   try {

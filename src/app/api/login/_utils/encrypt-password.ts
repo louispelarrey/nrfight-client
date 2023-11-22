415ea2646eb15903fa2cf906991b86eb66547b53
louispelarrey <louispelarrey@gmail.com>
2023-11-22 20:55:34 +0100

    Refactor encryption and decryption functions

--- src/app/api/login/_utils/encrypt-password.ts
+++ src/app/api/login/_utils/encrypt-password.ts
@@ -1,44 +1,22 @@
-const algorithm = {
-  name: 'AES-CTR',
-  counter: new Uint8Array(16), // this will be replaced with the IV for each operation
-  length: 128, // block size (in bits) for CTR mode, can be 64, 128
-};
+import crypto from 'crypto';
 
-const getKey = async (secret: string) => {
-  const encoder = new TextEncoder();
-  const keyData = encoder.encode(secret);
-  return await crypto.subtle.importKey(
-    'raw',
-    keyData,
-    algorithm,
-    false,
-    ['encrypt', 'decrypt']
-  );
-};
+const algorithm = 'aes-256-ctr'; // AES Counter Mode
+const iv = crypto.randomBytes(16); // Initialization vector
 
 export const encryptPassword = async (password: string): Promise<string> => {
-  if (!process.env.SECRET_KEY) throw new Error('SECRET_KEY is not defined');
+  if(!process.env.SECRET_KEY) throw new Error('SECRET_KEY is not defined');
 
-  const iv = crypto.getRandomValues(new Uint8Array(16));
-  algorithm.counter = iv;
-
-  const key = await getKey(process.env.SECRET_KEY);
-  const encoder = new TextEncoder();
-  const data = encoder.encode(password);
-  const encrypted = await crypto.subtle.encrypt(algorithm, key, data);
-
-  return Buffer.from(iv).toString('hex') + ':' + Buffer.from(encrypted).toString('hex');
+  const cipher = crypto.createCipheriv(algorithm, process.env.SECRET_KEY, iv);
+  const encrypted = Buffer.concat([cipher.update(password), cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
 };
 
 export const decryptPassword = async (encryptedPassword: string): Promise<string> => {
-  if (!process.env.SECRET_KEY) throw new Error('SECRET_KEY is not defined');
+  if(!process.env.SECRET_KEY) throw new Error('SECRET_KEY is not defined');
 
   const [ivHex, encrypted] = encryptedPassword.split(':');
-  algorithm.counter = Buffer.from(ivHex, 'hex');
-
-  const key = await getKey(process.env.SECRET_KEY);
-  const encryptedData = Buffer.from(encrypted, 'hex');
-  const decrypted = await crypto.subtle.decrypt(algorithm, key, encryptedData);
-
-  return new TextDecoder().decode(decrypted);
+  const ivBuffer = Buffer.from(ivHex, 'hex');
+  const decipher = crypto.createDecipheriv(algorithm, process.env.SECRET_KEY, ivBuffer);
+  const decrypted = Buffer.concat([decipher.update(Buffer.from(encrypted, 'hex')), decipher.final()]);
+  return decrypted.toString();
 };

ea184550a92c0054ba93ce9e8e12397d1709118c
louispelarrey <louispelarrey@gmail.com>
2024-02-11 16:53:52 +0100

    Refactor mergeReservedCourses function

--- src/lib/merge-reserved-courses.ts
+++ src/lib/merge-reserved-courses.ts
@@ -1,19 +1,32 @@
 import { IReservedCourse } from "@/app/api/reservation/_utils/save-reservation/save-reservation-to-db";
-import { ReservedCoursesPerSportigoRoom, ReservedCourse } from "@/providers/filter-provider";
+import {
+  ReservedCoursesPerSportigoRoom,
+  ReservedCourse,
+} from "@/providers/filter-provider";
 
-export function mergeReservedCourses(reservedCoursesPerSportigoRoom: ReservedCoursesPerSportigoRoom): IReservedCourse[] {
-  const reservedCourses = Object.entries(reservedCoursesPerSportigoRoom).map(([room, reservedCourses]) => {
-    return reservedCourses.map((reservedCourse: ReservedCourse) => {
-      if (!reservedCourse.sportigoId) return undefined;
+export function mergeReservedCourses(
+  reservedCoursesPerSportigoRoom: ReservedCoursesPerSportigoRoom
+): IReservedCourse[] {
+  const reservedCourses = Object.entries(reservedCoursesPerSportigoRoom).map(
+    ([room, reservedCourses]) => {
+      return reservedCourses.map((reservedCourse: ReservedCourse) => {
+        if (!reservedCourse.sportigoId) return undefined;
 
-      return {
-        sportigoId: reservedCourse.sportigoId,
-        room: room,
-        dayNumber: new Date(reservedCourse.startDate).getDay(),
-        hour: new Date(reservedCourse.startDate).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })
-      };
-    });
-  })
+        return {
+          sportigoId: reservedCourse.sportigoId,
+          room: room,
+          dayNumber: new Date(reservedCourse.startDate).getDay(),
+          hour: new Date(reservedCourse.startDate).toLocaleTimeString("fr-FR", {
+            hour: "2-digit",
+            minute: "2-digit",
+            timeZone: "Europe/Paris",
+          }),
+        };
+      });
+    }
+  );
 
-  return reservedCourses.flat().filter(course => course !== undefined) as IReservedCourse[];
+  return reservedCourses
+    .flat()
+    .filter((course) => course !== undefined) as IReservedCourse[];
 }
